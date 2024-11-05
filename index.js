1d77e2c7803356e5c4f761fe786a5ecf28f82863
clike92 <likecorey@gmail.com>
2024-11-04 20:28:02 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,101 +1,28 @@
-// === DO NOT EDIT THIS REGION ===
-// Read the comments to understand how the program is structured.
+//Unit Test concatOdds
+// expect ([3, 2, 1], [9, 1, 1, 1, 4, 15, -1]) to be [-1, 1, 3, 9, 15]
+// expect (arrays have only even numbers) to be (empty array)
+// expect (arrays with mixed numbers and strings) to be (arrays of integers but not numbers)
+// expect ([3, 2, 1], [9, 1, 1, 1, 4, 15, -1]) not to be [all numbers]
+// expect (input not arrays), to be (user prompted for arrays)
+// expect (user inputs arrays not of strings), to be (user prompted for integer array of string)
+// expect (user inputs only 1 array), to be (user prompted for two arrays)
 
-// Prompt the user for a list of integers separated by commas.
-const userInputString = prompt(
-  "Please enter some integers separated by commas.",
-  "1,2,3,4,5"
-);
+// //Functional Test
+// user can checkout as guest or registered user
+// ask the user if they would like to register/login or checkout as guest
+// shopping cart will remember if items were previously added or deleted
 
-// Split the string of numbers into an array of strings.
-const stringArray = userInputString.split(",");
+function concatOdd(arr1, arr2) {
+  const newArr = arr1.concat(arr2);
 
-// Convert the array of strings into an array of numbers.
-const numbers = [];
-for (let i = 0; i < stringArray.length; i++) {
-  const str = stringArray[i];
-  const number = parseInt(str);
-  numbers.push(number);
+  const newArr2 = newArr.filter((num, index) => {
+    if (num % 2 === 0) return false;
+    if (newArr.indexOf(num) !== index) return false;
+    return true;
+  });
+
+  return newArr2;
 }
 
-// Perform some calculations on the numbers.
-console.log(numbers);
-console.log(`You have given ${getLength(numbers)} numbers.`);
-console.log(`The sum of your numbers is ${getSum(numbers)}.`);
-console.log(`The mean of your numbers is ${getMean(numbers)}.`);
-console.log(`The smallest of your numbers is ${getMin(numbers)}.`);
-console.log(`The largest of your numbers is ${getMax(numbers)}.`);
-console.log(`The range of your numbers is ${getRange(numbers)}.`);
-console.log(`The even numbers you gave are ${getEvens(numbers)}.`);
-console.log(`The odd numbers you gave are ${getOdds(numbers)}.`);
-
-// === EDIT THE CODE BELOW ===
-// Complete the functions below to make the program work!
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number} the length of the array
- */
-function getLength(numbers) {
-  return numbers.length;
-}
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number} the sum of the numbers
- */
-function getSum(numbers) {
-  let sum = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    sum += numbers[i];
-  }
-  return sum;
-}
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number} the mean of the numbers
- */
-function getMean(numbers) {
-  return getSum(numbers) / getLength(numbers);
-}
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number} the smallest of the numbers
- */
-function getMin(numbers) {
-  return Math.min(...numbers);
-}
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number} the largest of the numbers
- */
-function getMax(numbers) {
-  return Math.max(...numbers);
-}
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number} the range of the numbers (max - min)
- */
-function getRange(numbers) {
-  return getMax(numbers) - getMin(numbers);
-}
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number[]} the even numbers in the array
- */
-function getEvens(numbers) {
-  return numbers.filter(number => number % 2 === 0);
-}
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number[]} the odd numbers in the array
- */
-function getOdds(numbers) {
-  return numbers.filter(number => number % 2 !== 0);
-}
+const result = concatOdd([3, 2, 1], [9, 1, 1, 1, 4, 15, -1]);
+console.log(result);
